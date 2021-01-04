83e528c1a07b4976d5e12bc39dc22f5a4409a30b
willwearing <willwearing@gmail.com>
2021-01-04 13:17:30 -0700

    lesson finished

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,17 +1,17 @@
 """
 Your task is create your own HashTable without using a built-in library.
-
+​
 Your HashTable needs to have the following functions:
-
+​
 - put(key, value) : Inserts a (key, value) pair into the HashTable. If the
 value already exists in the HashTable, update the value.
 - get(key): Returns the value to which the specified key is mapped, or -1 if
 this map contains no mapping for the key.
 - remove(key) : Remove the mapping for the value key if this map contains the
 mapping for the key.
-
+​
 Example:
-
+​
 ```plaintext
 hash_table = MyHashTable();
 hash_table.put("a", 1);
@@ -24,29 +24,93 @@ hash_table.remove("b");         // remove the mapping for 2
 hash_table.get("b");            // returns -1 (not found)
 ```
 """
-class ListNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
+# O(1)
+def hash_fn(key, length):
+    return id(key) % length
 
 class MyHashTable:
-    def __init__(self):
+    def __init__(self, capacity, hash_fn=hash_fn):
         # Your code here
-
-       
-    # Your code here
-
-    
-    
+        # init our storage with some positive number of empty slots 
+        self.storage = [None] * capacity
+        # Some function that takes a key and spits out an integer 
+        # we can make sure that the output is in bounds of our storage
+        # by using % 
+        self.hash_fn = hash_fn
+​
+    def print_storage(self):
+        print(self.storage)
+​
+    '''
+    Add the key and value as a pair in the hash table. 
+    If the value already exists in the HashTable, update the value.
+    '''
     def put(self, key, value):
         # Your code here
-
-
+        # 1. Run the key through our hash function 
+        # 2. set storage[index] = (key, value)
+        # This will have the effect of updating a key that already existed 
+        # in our hash table 
+        # Doesn't handle collisions at all 
+        index = self.hash_fn(key, len(self.storage))
+​
+        # check if this index is taken 
+            # if it is taken, check the incoming key against the current key 
+            # if they match, overwrite 
+            # if they don't, still overwrite, but also print a message saying
+            # we're overwriting 
+        if self.storage[index] is not None:
+            if self.storage[index][0] == key:
+                self.storage[index] = (key, value) # O(1)
+            else:
+                old_key, old_val = self.storage[index]
+                print(f"Collision! Overwriting {old_key}: {old_val}")
+                self.storage[index] = (key, value)
+​
+        self.storage[index] = (key, value)
+​
+    '''
+    Return the value associated with the given key.
+    If the key doesn't exist in the hash table, should return -1
+    '''
     def get(self, key):
         # Your code here
-
-
-    def remove(self, key: int) -> None:
+        # 1. Run our hash function on our key 
+        # 2. Check to see if the index is empty or not 
+        #   - if it is, return -1 
+        #   - otherwise, return the value 
+        index = self.hash_fn(key, len(self.storage))
+​
+        if self.storage[index] is None:
+            return -1
+​
+        return self.storage[index][1]
+​
+    '''
+    Removes the key-value pair specified by the key.
+    Set the spot where the key-value pair is to None.
+    Doesn't return anything.
+    '''
+    def remove(self, key) -> None:
         # Your code here
-
+        # 1. Run our hash function on our key 
+        # 2. Set self.storage[index] = None
+        index = self.hash_fn(key, len(self.storage))
+​
+        self.storage[index] = None # O(1)
+​
+​
+hash_table = MyHashTable(10)
+# ht.put('cat', 'dog')
+# ht.print_storage()
+# ht.put('cat', 'tiger')
+# ht.print_storage()
+​
+hash_table.put("a", 1)
+hash_table.put("b", 2)
+print(hash_table.get("a"))
+print(hash_table.get("c"))
+hash_table.put("b", 1)         
+print(hash_table.get("b"))
+hash_table.remove("b")         
+print(hash_table.get("b"))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -2,35 +2,53 @@
 You've uncovered a secret alien language. To your surpise, the language is made
 up of all English lowercase letters. However, the alphabet is possibly in a
 different order (but is some permutation of English lowercase letters).
-
+​
 You need to write a function that, given a sequence of words written in this
-secret language, and the order the alphabet, will determine if the given words
+secret language, and the order of the alien alphabet, will determine if the given words
 are sorted "alphabetically" in this secret language.
-
+​
 The function will return a boolean value, true if the given words are sorted
 "alphabetically" (based on the supplied alphabet), and false if they are not
 sorted "alphabetically".
-
+​
 Example 1:
-
+​
+{
+    0: 'h',
+    1: 'l',
+    2: 'a',
+    ...
+    18: 's',
+}
+​
 ```plaintext
 Input: words = ["lambda","school"], order = "hlabcdefgijkmnopqrstuvwxyz"
 Output: true
 Explanation: As 'l' comes before 's' in this language, then the sequence is
 sorted.
 ```
-
+​
+{
+    'h': 0,
+    'a': 1
+    ...
+    'e': 5,
+    ...
+    's': 18,
+    'w': 22,
+}
+​
 Example 2:
-
+​
 ```plaintext
 Input: words = ["were","where","yellow"], order = "habcdefgijklmnopqrstuvwxyz"
 Output: false
 Explanation: As 'e' comes after 'h' in this language, then words[0] > words[1],
 hence the sequence is unsorted.
 ```
-
+​
 Example 3:
-
+​
 ```plaintext
 Input: words = ["lambda","lamb"], order = "abcdefghijklmnopqrstuvwxyz"
 Output: false
@@ -39,9 +57,9 @@ shorter (in size.) According to lexicographical rules "lambda" > "lamb",
 because 'd' > '∅', where '∅' is defined as the blank character which is less
 than any other character (https://en.wikipedia.org/wiki/Lexicographic_order).
 ```
-
+​
 Notes:
-
+​
 - order.length == 26
 - All characters in words[i] and order are English lowercase letters.
 """
@@ -50,9 +68,59 @@ def are_words_sorted(words, alpha_order):
     Inputs:
     words: List[str]
     alpha_order: str
-
+​
     Output:
     bool
     """
     # Your code here
-
+    # if alpha_order is just the normal alphabet, then 
+    # we could just do a normal is_sorted check on the words in the 
+    # words list 
+​
+    # we need to figure out how to represent the alternate ordering 
+​
+    # we can keep track of the alternate ordering of letters with a 
+    # hash table where the keys are the letters and the values are 
+    # the letter's index in the alternate alphabet 
+​
+    # create the lookup table 
+    # doing the same thing using a comprehension 
+    # lookup_table = {letter: index for index, letter in enumerate(alpha_order)}
+    lookup_table = {} 
+​
+    for index, letter in enumerate(alpha_order):
+        lookup_table[letter] = index
+​
+    # do an is_sorted check but refer to the alternate ordering instead 
+    return is_sorted(words, lookup_table)
+​
+# checks to see if the words in the list are sorted according 
+# to the ordering specified by the lookup table 
+def is_sorted(words, lookup_table):
+    # iterate through our words list two at a time 
+    for i in range(1, len(words)):
+        word1 = words[i-1]
+        word2 = words[i]
+​
+        # iterate through the chars of the two words we're comparing 
+        # we only need to iterate up through the length of the smaller word 
+        length_of_shorter_word = min(len(word1), len(word2))
+​
+        for k in range(length_of_shorter_word):
+            # check that the current letter in the first word < the current letter 
+            letter1 = word1[k]
+            letter2 = word2[k]
+​
+            # of the next word, according to our lookup table 
+            if lookup_table[letter1] > lookup_table[letter2]:
+                return False 
+​
+            # if at any point this isn't the case, return False 
+            # otherwise, move on to the next char in each word
+​
+        # if we get outside this for loop without ever returning false 
+        # check if word1's length < word2's length 
+        if len(word1) > len(word2):
+            return False 
+​
+    return True 
